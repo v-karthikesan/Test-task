4942de46b58174f624b84b08815a9b63707e82b0
v-karthikesan <vkarthikesan@gmail.com>
2024-01-22 23:12:25 +0530

    Version 1

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name = "Rithika"
+days=15
+year=2023
+print("Dear",name)
+print("You have ",days,"days of balance for this Year(",year,")")
+
+
+# In[ ]:
+
+
+
+
